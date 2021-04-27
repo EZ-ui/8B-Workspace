956507cd475b458e93300beb6355e9462b014d6e
EZ-ui <75538206+EZ-ui@users.noreply.github.com>
2021-04-27 23:11:38 +0300

    Add files via upload

--- upload.php
+++ upload.php
@@ -1,23 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<link rel="stylesheet" href="html.css">
+
+	<title>Upload </title>
+</head>
+<body>
+	
+	<h4>Note : Please name the file/assignment as your name </h4>
+<nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+        </ul>
+    </nav>
+	<div class="file__upload">
+		<div class="header">
+			<p><i class="fa fa-cloud-upload fa-2x"></i><span><span>up</span>load</span></p>			
+		</div>
+		<form action="" method="POST" enctype="multipart/form-data" class="body">
+
+			<input type="file" name="file" id="upload" required>
+			<label for="upload">
+				<i class="fa fa-file-text-o fa-3x"></i>
+				<p>
+					<strong>Drag and drop</strong> files here<br>
+					or <span>browse</span> to begin the upload
+				</p>
+			</label>
+			<button name="submit" class="btn">Upload</button>
+		</form>
+	</div>
+	<tr>
+           <td>
+              <?php
+			  $conn = mysqli_connect('localhost','root','','upload-system');
+               $query2 = "SELECT * FROM filedownload ";
+               $run2 = mysqli_query($conn,$query2);
+               
+               while($rows = mysqli_fetch_assoc($run2)){
+                   ?>
+               <a href="download.php?file=<?php echo $rows['filename'] ?>">Download</a><br>
+               <?php
+               }
+               ?>
+           </td>
+       </tr>
+</body>
+</html>
+
 <?php
- 
-// This will return all files in that folder
-$files = scandir("uploads");
- 
-// If you are using windows, first 2 indexes are "." and "..",
-// if you are using Mac, you may need to start the loop from 3,
-// because the 3rd index in Mac is ".DS_Store" (auto-generated file by Mac)
-for ($a = 2; $a < count($files); $a++)
-{
-    ?>
-    <p>
-        <!-- Displaying file name !-->
-        <?php echo $files[$a]; ?>
- 
-        <!-- href should be complete file path !-->
-        <!-- download attribute should be the name after it downloads !-->
-        <a href="uploads/<?php echo $files[$a]; ?>" download="<?php echo $files[$a]; ?>">
-            Download
-        </a>
-    </p>
-    <?php
-}
+    $conn = mysqli_connect('localhost','root','','upload-system');
+    if(isset($_POST['submit'])){
+        $fileName = $_FILES['file']['name'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+        $path = "uploads/".$fileName;
+        
+        $query = "INSERT INTO filedownload(filename) VALUES ('$fileName')";
+        $run = mysqli_query($conn,$query);
+        
+        if($run){
+            move_uploaded_file($fileTmpName,$path);
+            echo "<script>alert('Thanks for the submission!')</script>";
+        }
+        else{
+            echo "error".mysqli_error($conn);
+        }
+        
+    }
+	
+	?>
+
